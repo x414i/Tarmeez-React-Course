f04ca255005731b9743e72dd37989d7182b58432
x414i <m.belaid@lamah.com>
2026-05-20 08:37:58 +0200

    (feat):  create file and add data to this file, and export componet

---
+++ src/MyFirstComponent.js
@@ -0,0 +1,11 @@
+function MyFirstComponent() {
+  return (
+    <div>
+      <h1>My First Component</h1>
+      <p>This is my first React component.</p>
+      <button>Click me!</button>
+    </div>
+  );
+}
+
+export default MyFirstComponent;
